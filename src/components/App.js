7249c7f626aafa47a650c58d69712ecd34ab0948
irfan-007 <thanatos786687@gmail.com>
2024-09-23 14:33:31 +0530

    done 0

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,49 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
 
+function App() {
+  const [list, setList] = useState([]);
+  const [user, setUser] = useState(false);
+  const [item, setItem] = useState("");
+  return (
+    <div>
+      <div id="current-user">
+        {user
+          ? "Current user:rohan, isAuthenticated: Yes"
+          : "Current user:, isAuthenticated: No"}
+      </div>
+      <button id="login-btn" onClick={() => setUser(true)}>
+        Login
+      </button>
+      <button id="signout" onClick={() => setUser(false)}>
+        Signout
+      </button>
+      <div>
+        <input
+          type="text"
+          id="shopping-input"
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button onClick={() => setList([...list, item])}>Add</button>
+      </div>
+      <button id="clear-list" onClick={() => setList([])}>
+        Clear
+      </button>
+      <ul>
+        {list.map((el, i) => (
+          <li key={i}>
+            <span id={`item-${el}`}>{el}</span>{" "}
+            <button
+              onClick={() => setList(list.filter((k) => k != el))}
+              style={{ color: "red" }}
+              id={`remove-${el}`}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
