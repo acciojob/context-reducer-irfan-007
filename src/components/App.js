f4fecf1da20caff0c6be699e0d8ae7a0da7014f7
irfan-007 <thanatos786687@gmail.com>
2024-09-23 15:02:22 +0530

    done 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,12 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+import Signin from "./Signin";
+export const myContext = createContext();
 
 function App() {
-  const [list, setList] = useState([]);
-  const [user, setUser] = useState(false);
-  const [item, setItem] = useState("");
   return (
-    <div>
-      <div id="current-user">
-        {user
-          ? "Current user:rohan, isAuthenticated: Yes"
-          : "Current user:, isAuthenticated: No"}
-      </div>
-      <button id="login-btn" onClick={() => setUser(true)}>
-        Login
-      </button>
-      <button id="signout" onClick={() => setUser(false)}>
-        Signout
-      </button>
-      <div>
-        <input
-          type="text"
-          id="shopping-input"
-          onChange={(e) => setItem(e.target.value)}
-        />
-        <button onClick={() => setList([...list, item])}>Add</button>
-      </div>
-      <button id="clear-list" onClick={() => setList([])}>
-        Clear
-      </button>
-      <ul>
-        {list.map((el, i) => (
-          <li key={i}>
-            <span id={`item-${el}`}>{el}</span>{" "}
-            <button
-              onClick={() => setList(list.filter((k) => k != el))}
-              style={{ color: "red" }}
-              id={`remove-${el}`}
-            >
-              X
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <myContext.Provider value={"rohan"}>
+      <Signin />
+    </myContext.Provider>
   );
 }
 
---
+++ src/components/Signin.js
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from "react";
+import { myContext } from "./App";
+
+function Signin() {
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState("");
+  const [userV, setUserV] = useState(false);
+  const user = useContext(myContext);
+  console.log(user);
+  return (
+    <div>
+      <div id="current-user">
+        {userV
+          ? `Current user:${user}, isAuthenticated: Yes`
+          : `Current user:${user}, isAuthenticated: No`}
+      </div>
+      <button id="login-btn" onClick={() => setUserV(true)}>
+        Login
+      </button>
+      <button id="signout" onClick={() => setUserV(false)}>
+        Signout
+      </button>
+      <div>
+        <input
+          type="text"
+          id="shopping-input"
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button onClick={() => setList([...list, item])}>Add</button>
+      </div>
+      <button id="clear-list" onClick={() => setList([])}>
+        Clear
+      </button>
+      <ul>
+        {list.map((el, i) => (
+          <li key={i}>
+            <span id={`item-${el}`}>{el}</span>{" "}
+            <button
+              onClick={() => setList(list.filter((k) => k != el))}
+              style={{ color: "red" }}
+              id={`remove-${el}`}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Signin;
